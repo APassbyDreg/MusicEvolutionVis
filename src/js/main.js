e2ec317f6f5e65032be02024acfb958180362441
zengbohan0217 <13811753844@126.com>
2021-05-23 01:43:39 +0800

    finish part of bar

--- src/js/main.js
+++ src/js/main.js
@@ -5,22 +5,22 @@ let appconf = {
             attr: 0,
             attrs: ["danceability", "energy", "valence", "tempo", "loudness", "mode", "key", "acousticness", "instrumentalness", "liveness", "speechiness", "explicit", "duration_ms", "popularity"],
             time_range: [1921, 2020],
-            graph_mode_list:["In", "Out", "Both"],
+            graph_mode_list: ["In", "Out", "Both"],
             graph_mode: 0,
             genres: ["Electronic", "R&B", "Vocal", "Pop/Rock", "Religious", "Blues", "Country", "Jazz", "Latin", "New Age", "Folk", "International", "Reggae", "Comedy/Spoken", "Easy Listening", "Classical", "Avant-Garde", "Stage & Screen", "Children's"],
             using_genres: Array(19).fill(true),
         }
     },
     methods: {
-        change_graph_mode: function (mode) {
+        change_graph_mode: function(mode) {
             this.graph_mode = mode;
             $("#graph-mode-switch button").removeClass("active");
             $("#graph-mode-switch button")[mode].classList.add("active");
         },
-        grene_change: function (idx) {
+        grene_change: function(idx) {
             console.log("genres", this.genres[idx], "toggled");
         },
-        range_change: function () {
+        range_change: function() {
             let start = (typeof(this.time_range[0]) === "number") ? this.time_range[0] : parseInt(this.time_range[0]);
             let end = (typeof(this.time_range[1]) === "number") ? this.time_range[1] : parseInt(this.time_range[1]);
             start = Math.max(1921, Math.min(2020, start));
@@ -33,7 +33,7 @@ let appconf = {
     computed: {
 
     },
-    mounted: function () {
+    mounted: function() {
         // initialize options
         // this.change_view_mode(0);
         this.change_graph_mode(0);
@@ -41,18 +41,13 @@ let appconf = {
 }
 
 let app = Vue.createApp(appconf)
-             .component("v-chart", VueECharts)
-             .component("v-slider", window['vue-slider-component'])
-             .mount("#app");
+    .component("v-chart", VueECharts)
+    .component("v-slider", window['vue-slider-component'])
+    .mount("#app");
 
 
 async function init_page() {
-    // example code
-    console.log("init ")
-    await readCSV("./assets/data/music_portion.csv");
-    portion_csv = window.__loaded_csv;
-    genre_list = portion_csv[0];
-    
+    await init_bar();
     await init_graph();
     await init_table();
 }
--- src/js/show_portion.js
+++ src/js/show_portion.js
@@ -2,52 +2,51 @@ var port_part = document.getElementById("graph-bar");
 var my_chart = echarts.init(port_part);
 
 var portion_csv;
-var genre_list;
-var now_start_time;
-var now_end_time;
+var genre_list = ["Electronic", "R&B", "Vocal", "Pop/Rock", "Religious", "Blues", "Country", "Jazz", "Latin", "New Age", "Folk", "International", "Reggae", "Comedy/Spoken", "Easy Listening", "Classical", "Avant-Garde", "Stage & Screen", "Children's"];
 
-port_option = {
-    color: ["#5470c6", "#91cc75", "#fac858"],
-    tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-            type: 'shadow'
-        }
-    },
-    legend: {
-        data: ['Direct', 'Mail Ad', 'Affiliate Ad', 'Video Ad', 'Search Engine'],
-        // selectorLabel: {
-        //     show: false
-        // },
-        // emphasis: {
-        //     selectorLabel: {
-        //         show: false
-        //     }
-        // }
-        show: false
-    },
-    grid: {
-        left: '-5%',
-        right: '-5%',
-        bottom: '0%',
-        top: '-8%',
-        containLabel: true
-    },
-    xAxis: {
-        type: 'category',
-        data: ['portion'],
-        axisLabel: {
+function draw_bra(start, end, portion_csv) {
+    port_option = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        legend: {
+            data: genre_list,
             show: false
+        },
+        grid: {
+            left: '-5%',
+            right: '-5%',
+            bottom: '0%',
+            top: '-8%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'category',
+            data: ['portion'],
+            axisLabel: {
+                show: false
+            }
+        },
+        yAxis: {
+            type: 'value',
+            axisLabel: {
+                show: false
+            }
+        },
+        series: []
+    };
+    for (var i = 0; i < genre_list.length; i++) {
+        var sum_num = 0;
+        for (var j = start - 1920; j <= end - 1920; j++) {
+            sum_num += 1;
+            // sum_num += portion_csv[i+1][j];
+            // bug is here
         }
-    },
-    yAxis: {
-        type: 'value',
-        axisLabel: {
-            show: false
-        }
-    },
-    series: [{
-            name: 'Direct',
+        new_part = {
+            name: genre_list[i],
             type: 'bar',
             stack: 'total',
             label: {
@@ -56,73 +55,18 @@ port_option = {
             emphasis: {
                 focus: 'series'
             },
-            data: [320]
-        },
-        {
-            name: 'Mail Ad',
-            type: 'bar',
-            stack: 'total',
-            label: {
-                show: false
-            },
-            emphasis: {
-                focus: 'series'
-            },
-            data: [120]
-        },
-        {
-            name: 'Affiliate Ad',
-            type: 'bar',
-            stack: 'total',
-            label: {
-                show: false
-            },
-            emphasis: {
-                focus: 'series'
-            },
-            data: [220]
-        },
-        {
-            name: 'Video Ad',
-            type: 'bar',
-            stack: 'total',
-            label: {
-                show: false
-            },
-            emphasis: {
-                focus: 'series'
-            },
-            data: [150]
-        },
-        {
-            name: 'Search Engine',
-            type: 'bar',
-            stack: 'total',
-            label: {
-                show: false
-            },
-            emphasis: {
-                focus: 'series'
-            },
-            data: [120]
-        }
-    ]
-};
-if (port_option && typeof port_option === 'object') {
-    my_chart.setOption(port_option);
+            data: [sum_num]
+        };
+        port_option["series"].push(new_part);
+    }
+    return port_option;
 }
 
-// function portion_time_change() {
-//     now_start_time = app.$data.time_range[0];
-//     now_end_time = app.$data.time_range[1];
-// }
-
-// let app_portion = {
-//     data() {
-//         return {
-//             year_range: [1921, 2020],
-//             start_year = 1921,
-//             end_year = 2020,
-//         }
-//     }
-// }
+async function init_bar() {
+    await readCSV("./assets/data/music_portion.csv");
+    portion_csv = window.__loaded_csv;
+    port_opt = draw_bra(1921, 2020, portion_csv);
+    if (port_opt && typeof port_opt === 'object') {
+        my_chart.setOption(port_opt);
+    }
+}
