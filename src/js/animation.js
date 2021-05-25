d015854325c53b708e71bb911a38c410e2bcf81e
Martin_Z_He <me@apassbydreg.work>
2021-05-25 11:23:03 +0800

    add danmaku

---
+++ src/js/animation.js
@@ -0,0 +1,25 @@
+var animation_timer;
+var animation_state;
+
+function start_animation(animation, speed) {
+    animation_timer = setInterval(animation, speed)
+}
+
+function time_animation() {
+    if (!animation_state) {
+        app.time_range = [1921, 1930];
+        app.range_change();
+        animation_state = true;
+    }
+    else {
+        if (app.time_range[1] != 2020) {
+            app.time_range[0] += 1;
+            app.time_range[1] += 1;
+            app.range_change();
+        }
+        else {
+            animation_state = false;
+            clearInterval(animation_timer);
+        }
+    }
+}
--- src/js/loader.js
+++ src/js/loader.js
@@ -26,3 +26,33 @@ async function readCSV (url) {
         }
     });
 }
+
+// for vue component loader
+const vue_loader_options = {
+      moduleCache: {
+        vue: Vue
+      },
+      async getFile(url) {
+        
+        // v0.7.x
+        const res = await fetch(url);
+        if ( !res.ok )
+          throw Object.assign(new Error(res.statusText + ' ' + url), { res });
+        return await res.text();
+
+        /* v0.8.x 
+        const res = await fetch(url);
+        if ( !res.ok )
+          throw Object.assign(new Error(res.statusText + ' ' + url), { res });
+        return {
+          getContentData: asBinary => asBinary ? res.arrayBuffer() : res.text(),
+        }
+        */
+      },
+      addStyle(textContent) {
+
+        const style = Object.assign(document.createElement('style'), { textContent });
+        const ref = document.head.getElementsByTagName('style')[0] || null;
+        document.head.insertBefore(style, ref);
+      },
+    }
