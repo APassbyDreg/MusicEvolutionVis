981e94d99f2ceee716243023b9ca839ddca05f7a
Martin Z He <me@apassbydreg.work>
2021-06-14 12:34:29 +0800

    add more color control

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ let appconf = {
             attrs: ["Danceability", "Energy", "Valence", "Tempo", "Loudness", "Mode", "Key", "Acousticness", "Instrumentalness", "Liveness", "Speechiness", "Explicit", "Duration", "Popularity"],
             table_mode_list: ["Attrs", "Timeline"],
             genres: ["Electronic", "R&B", "Vocal", "Pop/Rock", "Religious", "Blues", "Country", "Jazz", "Latin", "New Age", "Folk", "International", "Reggae", "Comedy/Spoken", "Easy Listening", "Classical", "Avant-Garde", "Stage&Screen", "Children's"],
-            genre_colors: ['#f09090', '#dea487', '#f0d090', '#d6d675', '#d0f090', '#93da70', '#90f090', '#83d9a0', '#90f0d0', '#75d2d2', '#90d0f0', '#6c8fd5', '#9090f0', '#9070d1', '#d090f0', '#e184e1', '#f090d0', '#dc87a3', '#c0c0c0'],
+            genre_colors_raw: [[0.0, 0.75, 0.75], [0.06, 0.56, 0.65], [0.11, 0.75, 0.75], [0.17, 0.59, 0.66], [0.22, 0.75, 0.75], [0.28, 0.52, 0.63], [0.33, 0.75, 0.75], [0.39, 0.56, 0.69], [0.44, 0.75, 0.75], [0.5, 0.5, 0.62], [0.56, 0.75, 0.75], [0.61, 0.53, 0.61], [0.67, 0.75, 0.75], [0.72, 0.54, 0.62], [0.78, 0.75, 0.75], [0.83, 0.57, 0.68], [0.89, 0.75, 0.75], [0.94, 0.59, 0.65], [0, 0, 0.75]],
             musicians: [],
             // APP 状态值
             fullscreen: 0, // 0: 不显示, 1: 全屏显示表格, 2: 全屏显示图
@@ -151,7 +151,16 @@ let appconf = {
             }
         },
     },
-    computed: {},
+    computed: {
+        genre_colors: function () {
+            let colors = [];
+            this.genre_colors_raw.forEach((hsl) => {
+                let rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
+                colors.push(`#${rgb[0].toString(16)}${rgb[1].toString(16)}${rgb[2].toString(16)}`);
+            });
+            return colors;
+        }
+    },
     mounted: function() {
         // initialize options
         $("#table-mode-switch button")[0].classList.add("active");
